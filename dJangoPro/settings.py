1818955b961e91014b18e503b77d8a5105655753
cassianoczz <cassianoczz@users.noreply.github.com>
2022-07-26 18:09:59 -0300

    Teste AWS Heroku

--- dJangoPro/settings.py
+++ dJangoPro/settings.py
@@ -136,34 +136,34 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'mediafiles')
 
-AWS_ACCESS_KEY_ID = config('AWS_ACCESS_KEY_ID')
-
+# AWS_ACCESS_KEY_ID = config('AWS_ACCESS_KEY_ID')
+#
 # STORAGE CONFIGURATION IN S3 AWS
-if AWS_ACCESS_KEY_ID:
-    AWS_SECRET_ACCESS_KEY = config('AWS_SECRET_ACCESS_KEY')
-    AWS_STORAGE_BUCKET_NAME = config('AWS_STORAGE_BUCKET_NAME')
-    AWS_S3_OBJECT_PARAMETERS = {'CacheControl': 'max-age=86400', }
-    AWS_PRELOAD_METADATA = True
-    AWS_AUTO_CREATE_BUCKET = False
-    AWS_QUERYSTRING_AUTH = False
-
-    AWS_DEFAULT_ACL = None
-
-    # Static Assets
-    STATICFILES_STORAGE = 's3_folder_storage.s3.StaticStorage'
-    STATIC_S3_PATH = 'static'
-    STATIC_ROOT = f'/{STATIC_S3_PATH}/'
-    STATIC_URL = f'//s3.amazonaws.com/{AWS_STORAGE_BUCKET_NAME}/{STATIC_S3_PATH}/'
-    ADMIN_MEDIA_PREFIX = STATIC_URL + 'admin/'
+# if AWS_ACCESS_KEY_ID:
+#     AWS_SECRET_ACCESS_KEY = config('AWS_SECRET_ACCESS_KEY')
+#     AWS_STORAGE_BUCKET_NAME = config('AWS_STORAGE_BUCKET_NAME')
+#     AWS_S3_OBJECT_PARAMETERS = {'CacheControl': 'max-age=86400', }
+#     AWS_PRELOAD_METADATA = True
+#     AWS_AUTO_CREATE_BUCKET = False
+#     AWS_QUERYSTRING_AUTH = False
+#
+#     AWS_DEFAULT_ACL = None
+#
+#     Static Assets
+    # STATICFILES_STORAGE = 's3_folder_storage.s3.StaticStorage'
+    # STATIC_S3_PATH = 'static'
+    # STATIC_ROOT = f'/{STATIC_S3_PATH}/'
+    # STATIC_URL = f'//s3.amazonaws.com/{AWS_STORAGE_BUCKET_NAME}/{STATIC_S3_PATH}/'
+    # ADMIN_MEDIA_PREFIX = STATIC_URL + 'admin/'
 
     # Media Assets
-    MEDIAFILES_STORAGE = 's3_folder_storage.s3.StaticStorage'
-    MEDIA_S3_PATH = 'media'
-    MEDIA_ROOT = f'/{MEDIA_S3_PATH}/'
-    MEDIA_URL = f'//s3.amazonaws.com/{AWS_STORAGE_BUCKET_NAME}/{MEDIA_S3_PATH}/'
-
-    INSTALLED_APPS.append('s3_folder_storage')
-    INSTALLED_APPS.append('storages')
+    # MEDIAFILES_STORAGE = 's3_folder_storage.s3.StaticStorage'
+    # MEDIA_S3_PATH = 'media'
+    # MEDIA_ROOT = f'/{MEDIA_S3_PATH}/'
+    # MEDIA_URL = f'//s3.amazonaws.com/{AWS_STORAGE_BUCKET_NAME}/{MEDIA_S3_PATH}/'
+    #
+    # INSTALLED_APPS.append('s3_folder_storage')
+    # INSTALLED_APPS.append('storages')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
