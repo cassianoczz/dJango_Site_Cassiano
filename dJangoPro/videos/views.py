b58f7439d41fd82153b8a6357aab62d09b757cba
cassianoczz <cassianoczz@users.noreply.github.com>
2022-07-31 10:05:42 -0300

    Criado uma listagem de videos HardCode

--- dJangoPro/videos/views.py
+++ dJangoPro/videos/views.py
@@ -2,5 +2,9 @@ from django.shortcuts import render
 
 
 def videos(request, slug):
-    video = {'titulo': 'Video: Apresentação', 'vimeo_id': 251224475}
+    videos = {
+        'apresentacao':{'titulo': 'Video: Apresentação', 'vimeo_id': 251224475},
+        'instalacao-windows': {'titulo': 'Video: Instalacao Windows', 'vimeo_id': 251497668}
+    }
+    video = videos[slug]
     return render(request, 'videos/video.html', context={'video': video})
